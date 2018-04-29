6790fd0c44f4260d8901a639e09c77ce303a5eb4
Felix Pojtinger <felix@pojtinger.com>
2018-04-30 01:13:10 +0200

    Added basic queries and muations for listing, up- and downvoting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Routes } from "./routes/Routes";
 import "semantic-ui-css/semantic.min.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000"
+});
 
 ReactDOM.render(
-  <Routes />,
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
   document.getElementById("direct-democracy-frontend")
 );
