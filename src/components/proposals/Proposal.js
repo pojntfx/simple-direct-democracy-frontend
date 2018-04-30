6fbb667f026181bae8f802a9d26b4f5b28e4c47f
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-04-30 18:53:25 +0200

    Added private IP to context using WebRTC

--- src/components/proposals/Proposal.js
+++ src/components/proposals/Proposal.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Icon, Card } from "semantic-ui-react";
 import styled from "styled-components";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
+import { getIPs } from "../../utils/ip";
 
 const UPVOTE_PROPOSAL = gql`
-  mutation($id: String!) {
-    upvoteProposal(id: $id) {
+  mutation($id: String!, $author: String!) {
+    upvoteProposal(id: $id, author: $author) {
       votes
     }
   }
@@ -39,46 +40,69 @@ const CardButton = styled(Button)`
   white-space: nowrap;
 `;
 
-export const Proposal = ({ children, votes, id, ...otherProps }) => (
-  <Card fluid {...otherProps}>
-    <Card.Content>
-      <Card.Header>{children}</Card.Header>
-    </Card.Content>
-    <Card.Content extra>
-      <CardActions>
-        <Mutation mutation={UPVOTE_PROPOSAL}>
-          {upvoteProposal => (
-            <CardButton
-              basic
-              color="green"
-              onClick={() => upvoteProposal({ variables: { id } })}
-            >
-              <Icon name="arrow up" />
-              Upvote
-            </CardButton>
-          )}
-        </Mutation>
-        <span>
-          <b>{votes}</b>
-        </span>
+export class Proposal extends Component {
+  state = {
+    ip: ""
+  };
 
-        <Mutation mutation={DOWNVOTE_PROPOSAL}>
-          {downvoteProposal => (
-            <CardButton
-              basic
-              color="red"
-              onClick={() =>
-                downvoteProposal({ variables: { id, author: "me" } }).catch(
-                  error => alert(error)
-                )
-              }
-            >
-              <Icon name="arrow down" />
-              Downvote
-            </CardButton>
-          )}
-        </Mutation>
-      </CardActions>
-    </Card.Content>
-  </Card>
-);
+  componentDidMount() {
+    getIPs(
+      ips =>
+        ips.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/)
+          ? this.setState({ ip: ips })
+          : null
+    );
+  }
+
+  render() {
+    const { children, votes, id, ...otherProps } = this.props;
+    const { ip } = this.state;
+    return (
+      <Card fluid {...otherProps}>
+        <Card.Content>
+          <Card.Header>{children}</Card.Header>
+        </Card.Content>
+        <Card.Content extra>
+          <CardActions>
+            <Mutation mutation={UPVOTE_PROPOSAL}>
+              {upvoteProposal => (
+                <CardButton
+                  basic
+                  color="green"
+                  onClick={() =>
+                    upvoteProposal({ variables: { id, author: ip } }).catch(
+                      error => alert(error)
+                    )
+                  }
+                >
+                  <Icon name="arrow up" />
+                  Upvote
+                </CardButton>
+              )}
+            </Mutation>
+            <span>
+              <b>{votes}</b>
+            </span>
+
+            <Mutation mutation={DOWNVOTE_PROPOSAL}>
+              {downvoteProposal => (
+                <CardButton
+                  basic
+                  color="red"
+                  onClick={() =>
+                    downvoteProposal({ variables: { id, author: ip } }).catch(
+                      error => alert(error)
+                    )
+                  }
+                >
+                  <Icon name="arrow down" />
+                  Downvote
+                </CardButton>
+              )}
+            </Mutation>
+          </CardActions>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
